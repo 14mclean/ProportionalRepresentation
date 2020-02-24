e1b22ddfaba1eccc0f7a5beb8eb89845c46d3940
14mclean <60158444+14mclean@users.noreply.github.com>
2020-02-24 14:57:18 +0000

    Update driver.java

--- driver.java
+++ driver.java
@@ -6,7 +6,8 @@ public class driver
     {
         Scanner scan = new Scanner(System.in);
         int numPeople = 0, numOptions = 0;
-        
+        String[] names = new String[numPeople];
+
         // Collect num of people, different options & people's orders
 
         System.out.println("How many people: ");
@@ -16,5 +17,34 @@ public class driver
         System.out.println("How many choices: ");
         numOptions = scan.nextInt();
         String[] options = new String[numOptions];
+
+        for(int count = 0; count < numOptions; count++)
+        {
+            System.out.println("What is option " + count + ": ");
+            options[count] = new String(scan.next());
+        }
+
+        for(int count = 0; count < numPeople; count++)
+        {
+            System.out.println("\nWho is person " + count + ": ");
+            people[count] = new Person(scan.next(), numOptions);
+
+            for(int count2 = 0; count2 < numOptions; count2++)
+            {
+                System.out.println("What place is " + options[count2] + ": ");
+                people[count].setChoice(options[count2], scan.nextInt()-1);
+            }
+        }
+
+        for(int count = 0; count < numPeople; count++)
+        {
+            System.out.println("Does " + people[count].getName() + "have a veto (Y/N)");
+            if(scan.next().toUpperCase() == "Y")
+            {
+                System.out.println("What is " + people[count] + "'s veto:");
+                people[count].setVeto(scan.next());
+                System.out.println("\n");
+            }
+        }
     }
 }
