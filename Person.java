bc20d04f00f11430c9b12d6fc4eb721963e407be
14mclean <60158444+14mclean@users.noreply.github.com>
2020-02-24 14:57:23 +0000

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,17 +1,31 @@
 class Person
 {
     private String[] choiceOrder;
-    private String name = new String();
+    private String name = new String(), veto = new String();
 
-    Person(String name, String[] choiceOrder)
+    Person(String name, int numChoices)
     {
         this.name = name;
-        this.choiceOrder = new String[choiceOrder.length];
-        this.choiceOrder = choiceOrder;
+        this.choiceOrder = new String[numChoices];
     }
 
     String getName()
     {
         return name;
+    }
+
+    void setVeto(String veto)
+    {
+        this.veto = veto;
+    }
+
+    String getVeto()
+    {
+        return veto;
+    }
+
+    void setChoice(String choice, int place)
+    {
+        this.choiceOrder[place] = choice;
     }
 }
