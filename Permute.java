436c92af7e26c36d9fc77e66fd955ade216c377e
14mclean <60158444+14mclean@users.noreply.github.com>
2020-02-24 17:32:47 +0000

    Create Permute.java

---
+++ Permute.java
@@ -0,0 +1,16 @@
+public class Permute
+{
+    static void permute(java.util.List<String> arr, int k)
+    {
+        for(int i = k; i < arr.size(); i++)
+        {
+            java.util.Collections.swap(arr, i, k);
+            permute(arr, k+1);
+            java.util.Collections.swap(arr, k, i);
+        }
+        if (k == arr.size() -1)
+        {
+            System.out.println(java.util.Arrays.toString(arr.toArray()));
+        }
+    }
+}
