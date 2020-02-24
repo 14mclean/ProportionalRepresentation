8fb5ef84eb2f0d866350cc7845955de2a9723563
14mclean <60158444+14mclean@users.noreply.github.com>
2020-02-24 14:23:26 +0000

    Update driver.java

--- driver.java
+++ driver.java
@@ -1,9 +1,20 @@
+import java.util.*;
+
 public class driver
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+        int numPeople = 0, numOptions = 0;
+        
         // Collect num of people, different options & people's orders
 
-        //
+        System.out.println("How many people: ");
+        numPeople = scan.nextInt();
+        Person[] people = new Person[numPeople];
+
+        System.out.println("How many choices: ");
+        numOptions = scan.nextInt();
+        String[] options = new String[numOptions];
     }
 }
