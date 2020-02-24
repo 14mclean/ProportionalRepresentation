cfaefa1582ee9b1126c79bc805c50ce973eb9a5d
14mclean <60158444+14mclean@users.noreply.github.com>
2020-02-24 17:32:45 +0000

    Update driver.java

--- driver.java
+++ driver.java
@@ -2,6 +2,41 @@ import java.util.*;
 
 public class driver
 {
+    static void permute(List<Person> arr, int k, List<Person[]> out)
+    {
+        for(int i = k; i < arr.size(); i++)
+        {
+            Collections.swap(arr, i, k);
+            permute(arr, k+1, out);
+            Collections.swap(arr, k, i);
+        }
+        if (k == arr.size() -1)
+        {
+            //out.add(arr.toArray());
+            Person[] temp = new Person[arr.size()];
+            for(int count = 0; count < arr.size(); count++)
+            {
+                temp[count] = arr.get(count);
+            }
+        }
+    }
+
+    static int factorial(int n)
+    {
+        int result = 1;
+
+        for(int fact = 2; fact <= n; fact++)
+        {
+            result *= fact;
+        }
+
+        return result;
+    }
+
+    static int P(int n, int r)
+    {
+        return factorial(n)/factorial(n-r);
+    }
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
@@ -45,6 +80,39 @@ public class driver
                 people[count].setVeto(scan.next());
                 System.out.println("\n");
             }
+        }
+
+        System.out.println("\n-----Starting selection process-----\n");
+
+        List<Person> temp = new ArrayList<Person>();
+        List<Person[]> end = new ArrayList<Person[]>();
+        for(int count = 0; count < numPeople; count++)
+        {
+            temp.add(people[count]);
+        }
+
+        permute(temp, 0, end);
+
+        int topHappiness = 0;
+        Person[] topHappinessP = new Person[numPeople];
+        
+        for(int count = 0, happiness = 0; count < end.size(); count++)
+        {
+            happiness = 0;
+            for(int i = 0; i < numPeople; i++)
+            {
+                happiness += end.get(count)[i].getHappiness(options[i]);
+            }
+            if(happiness > topHappiness)
+            {
+                topHappinessP = end.get(count);
+            }
+        }
+
+        System.out.println("-----Results-----");
+        for(int count = 0; count < numPeople; count++)
+        {
+            System.out.println(options[count] + topHappinessP[count].getName());
         }
     }
 }
